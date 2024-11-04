4101d9ef2d7cf664e23b4ee238765bcbba7aa152
fanny-elimar <137103853+fanny-elimar@users.noreply.github.com>
2024-11-04 15:12:59 +0100

    changement titre

--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@ $recipes=getRecipes($pdo);
             <?php } ?>
             </div>
 
-      <h1 class="text-primary text-center">Mes recettes sans gluten</h1>
+      <h1 class="text-primary text-center">Mes recettes sans gluten super bonnes!</h1>
       <div class="container  ">
         <div class="row gap-3 justify-content-center">
         <?php foreach ($recipes as $recipe) {?>
